67ebdf36d8d04fec99fa1cec54b9184a0b836a7d
HoangHuy-23 <hh.glorious@gmail.com>
2025-01-15 21:16:32 +0700

    Update BASE_URL to point to the backend server

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -5,7 +5,7 @@ import toast from "react-hot-toast";
 import axios from "axios";
 import { io, Socket } from "socket.io-client";
 
-const BASE_URL = "https://chat-video-calling-app-fe.onrender.com";
+const BASE_URL = "https://chat-video-calling-app-be.onrender.com";
 
 interface iAuthStore {
   user: User | null;
